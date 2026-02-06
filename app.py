f6568dc950f9b52b32359f8adcf841efc4553338
patricksibiya115 <patricksibiya115@gmail.com>
2026-02-06 14:43:14 +0200

    Windows compatibility fix

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ import streamlit as st
 import streamlit as st
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain_community.vectorstores import Chroma
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.chains import RetrievalQA
 from langchain_community.document_loaders import PyPDFLoader
 import os
